e2f552ee9b2065ab5ec599452bfe4df46ff8c3f6
friedinger <friedinger@unisoftwareplus.com>
2021-07-27 11:06:51 +0200

    add reproduction code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-assets-rc';
+
+  constructor(private _http: HttpClient) {
+    this._http.get('assets/config.json').pipe(tap(r => console.log(JSON.stringify(r)))).toPromise();
+  }
 }
